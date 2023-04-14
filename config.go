db1deb8741660732408988218002eea8cc3e249f
caixw <xnotepad@gmail.com>
2023-04-14 21:59:52 +0800

    feat: 添加 WDDir 和 New

--- config.go
+++ config.go
@@ -22,7 +22,7 @@ type Config struct {
 // dir 相对的目录名称；
 // 根据 [os.UserConfigDir] 定位目录。
 func SystemDir(dir string) (*Config, error) {
-	return newConfig(dir, os.UserConfigDir)
+	return New(dir, os.UserConfigDir)
 }
 
 // AppDir 将应用程序下的 dir 作为配置文件的保存目录
@@ -30,16 +30,27 @@ func SystemDir(dir string) (*Config, error) {
 // dir 相对的目录名称；
 // 根据 [os.Executable] 定位目录。
 func AppDir(dir string) (*Config, error) {
-	return newConfig(dir, os.Executable)
+	return New(dir, func() (string, error) {
+		ex, err := os.Executable()
+		return filepath.Dir(ex), err
+	})
 }
+
+// WDDir 将工作目录作为配置文件的保存目录
+func WDDir(dir string) (*Config, error) { return New(dir, os.Getwd) }
 
 // Dir 以指定的目录作为配置文件的保存位置
 func Dir(dir string) *Config {
-	c, _ := newConfig(dir, nil)
+	c, _ := New(dir, nil)
 	return c
 }
 
-func newConfig(dir string, parent func() (string, error)) (*Config, error) {
+// New 声明 Config 对象
+//
+// dir 表示当前项目的配置文件存放的目录名称，一般和项目名称相同；
+// parent 表示获取系统中用于存放配置文件的目录，比如 Linux 中的 XDG_CONFIG 等目录。
+// 用户可以根据自己的需求自行实现该方法，如果为 nil，表示直接将 dir 作为全路径进行处理。
+func New(dir string, parent func() (string, error)) (*Config, error) {
 	if parent != nil {
 		p, err := parent()
 		if err != nil {
