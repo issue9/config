c5f9bd37cb5856eeba30d874a301030fc7d939ef
caixw <xnotepad@gmail.com>
2024-10-15 21:06:59 +0800

    refactor: 更新依赖项调整了本地化文件的格式

--- config.go
+++ config.go
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 //go:generate web locale -l=und -f=yaml ./
-//go:generate web update-locale -src=./locales/und.yaml -dest=./locales/cmn-Hans.yaml
+//go:generate web update-locale -src=./locales/und.yaml -dest=./locales/zh.yaml
 
 // Package config 提供了对多种格式配置文件的支持
 package config
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/issue9/config
 
-go 1.21
+go 1.23.0
 
 require (
-	github.com/issue9/assert/v4 v4.3.0
-	github.com/issue9/localeutil v0.27.2
-	golang.org/x/text v0.18.0
+	github.com/issue9/assert/v4 v4.3.1
+	github.com/issue9/localeutil v0.28.0
+	golang.org/x/text v0.19.0
 )
